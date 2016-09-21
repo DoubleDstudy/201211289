26720e78b2a721086d2fd57bc10e697437415f3b
201211289/유성민 <butter647@hanmail.net>
2016-09-21 14:29:04 +0900

    Add files via upload

---
+++ 201211289_2_4.java
@@ -0,0 +1,11 @@
+class Person{
+private float weight;
+public void setWeight(float w){
+this.weight=w;
+}
+private float getWeight(){
+return weight;
+}
+}
+Person p = new Person();
+p.setWeight((float)63.3);
