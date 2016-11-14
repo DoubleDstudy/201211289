14aaf31b24c24cd0b52ebfdc4520dbc970aff83a
201211289/유성민 <butter647@hanmail.net>
2016-11-14 18:56:04 +0900

    Add files via upload

---
+++ 20161114_Subject/p2_TurtleTrack.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import ch.aplu.turtle.*;
+class p2_TurtleTrack{   //TurtleTrack
+static void turtleTrack(){
+Turtle t1 = new Turtle();
+ArrayList<Double[]> posArr = new ArrayList<Double[]>();
+for(int i =0;i<4;i++){
+Double[] pos = new Double[2];
+    pos[0] = t1.getX();
+    pos[1] = t1.getY();
+    posArr.add(pos);
+    t1.forward(100);
+    t1.right(90);
+}
+for(Double[] d : posArr){
+System.out.printf("(%.1f,%.1f)",d[0],d[1]);
+}
+}
+   public static void main(String[] args) {
+   turtleTrack();
+   }
+}
